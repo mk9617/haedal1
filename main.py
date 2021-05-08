a7a56b37fe9f6e30be47036ed7c545dda5a7e62a
김민관 <mkk98@naver.com>
2021-05-09 00:11:53 +0900

    3주차 과제

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+class player:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __init__(self,name,age,team,num):
+        self.name = name
+        self.age = age
+        self.team = team
+        self.num = num
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def info(self):
+        print(f'이름: {self.name}')
+        print(f'나이: {self.age}')
+        print(f'팀: {self.team}')
+        print(f'등번호:{self.num}')
+        print()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+player1 = player('김상수',32,'삼성 라이온즈',7)
+player2 = player('이대호',40,'롯데 자이언츠',10)
+player3 = player('소형준',21,'KT WIZ',30)
+
+player1.info()
+player2.info()
+player3.info()
+
